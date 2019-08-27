6fae957a930816ccf51d7eea7cb573ea0889d4ac
Pravalik Medi [HATA SysOps] <Pravalik.Medi@hyundai-hata.com>
2019-08-27 14:18:46 -0700

    Week1 script

---
+++ Capstone Final.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <H1> Capstone Final Project</H1> 
+
+# <i>This project will mainly used for capstone Project</i>
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[4]:
+
+
+import numpy  as np
+
+
+# In[6]:
+
+
+print('Hello Capstone Project Course!')
+
+
+# In[ ]:
+
+
+
+
